d8cd59926d44cd5c8c20fafed2efa41af7f8ed20
barre <barrenac@oregonstate.edu>
2023-10-05 11:19:57 -0700

    completed portfolio.js page

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <br/>
+      <h3>Horiseon webpage</h3>
+      <a href="https://barcar95.github.io/horiseon-accessibility/"><img src="/horiseon-webpage-screenshot.jpg" alt="horiseon webpage screenshot"/></a>
+      <br/>
+      <h3>Comendo</h3>
+      <a href="https://barcar95.github.io/comendo/"><img src="/logo3.png" alt="Comendo app"/></a>
+      <br/>
+      <h3>Gym Tracker App</h3>
+      <a href="https://gym-trackerr-c227a2e45128.herokuapp.com/"><img src="/1-majin-buu-vs-patrick-funny-anime-motivational-gym-matthew-chan.jpg" alt="Gym Tracker app"/></a>
+      <br/>
+      <h3>Travel App</h3>
+      <a href="https://www.google.com/"><img src="/travel.jpg" alt="travel app"/></a>
+      <br/>
+      <h3>Anime App</h3>
+      <a href="https://www.google.com/"><img src="/goku-luffy.JPG" alt="Anime app"/></a>
+      <br/>
+      <h3>Calendar App</h3>
+      <a href="https://www.google.com/"><img src="/calendar.png" alt="calendar app"/></a>
+    </div>
+  );
+}
