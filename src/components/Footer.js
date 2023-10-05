9b138468e23726094fd884758898195923ca469f
barre <barrenac@oregonstate.edu>
2023-10-05 13:49:38 -0700

    sourced images with period

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
   return (
     <footer className="">
       <div className="">
-        <a href="https://github.com/barcar95"><img src="/github-icon.webp" alt="github icon"/></a>
-        <a href="https://www.linkedin.com/in/carlos-barrena-629b3820b/"><img src="/linkedin-icon.png" alt="linkedIn icon"/></a>
-        <a href="https://leetcode.com/barcar0812/"><img src="/leetcode-icon.webp" alt="Leetcode icon"/></a>
+        <a href="https://github.com/barcar95"><img src="./github-icon.webp" alt="github icon"/></a>
+        <a href="https://www.linkedin.com/in/carlos-barrena-629b3820b/"><img src="./linkedin-icon.png" alt="linkedIn icon"/></a>
+        <a href="https://leetcode.com/barcar0812/"><img src="./leetcode-icon.webp" alt="Leetcode icon"/></a>
       </div>
     </footer>
   );
