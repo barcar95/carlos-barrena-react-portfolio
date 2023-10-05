61ba3168e40b16953c9c40692038f153466fe779
barre <barrenac@oregonstate.edu>
2023-10-05 11:44:37 -0700

    Added downloadable link to resume.js page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,8 +4,7 @@ export default function About() {
   return (
     <div>
       <h1>About Me</h1>
-      <img src="/me.jpeg" alt="photo of Carlos Barrena"/>
-
+      <img src="/me.jpeg" alt="Carlos Barrena"/>
       <p>
         Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
         Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
---
+++ src/components/pages/Resume.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div>
+       <a href="/Carlos-Barrena-Resume.pdf" download><img src="/download-icon.png" alt="download icon"/>Download Resume</a> 
+    </div>
+  );
+}
