adec310d76ac56373e0667271999f1c60c51a4c1
barre <barrenac@oregonstate.edu>
2023-10-05 11:08:31 -0700

    completed About.js page

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="r">
+      <div className="">
+        <h1 className="" >
+          Carlos Barrena Portfolio
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
